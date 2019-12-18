163e92ca5da931981e28fe818548fce17850d48c
sampila <alipxsulistio@gmail.com>
2019-12-18 14:39:02 +0700

    added travis ci

--- randomstr/randomstr_test.go
+++ randomstr/randomstr_test.go
@@ -2,12 +2,20 @@ package randomstr
 
 import(
   "testing"
+  "os"
   "github.com/stretchr/testify/assert"
 )
 
-func TestRandStringRunes(t *testing.T){
+func TestMain(m *testing.M){
+  os.Exit(m.Run())
+}
+
+func TestRandStringRunesMinusLength(t *testing.T){
 	str1 := RandStringRunes(-1)
-	str2 := RandStringRunes(6)
   assert.EqualValues(t, str1, "")
-	assert.EqualValues(t, 6, len(str2))
+}
+
+func TestRandStringRunesValidLength(t *testing.T){
+  str2 := RandStringRunes(6)
+  assert.EqualValues(t, 6, len(str2))
 }
